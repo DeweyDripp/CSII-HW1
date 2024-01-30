17bb353bcf8ef538a2b55839a9b6a27d17d5fb05
DeweyDripp <ryanjay120504@gmail.com>
2024-01-29 21:08:29 -0600

    Initial Commit

---
+++ HW 1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HW 1
+//
+//  Created by Ryan Ramirez on 1/29/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
